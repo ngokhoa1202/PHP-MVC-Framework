1b092d8e77b5d324f46f93fb9b62642b8126392a
Khoa Ngo <khoa.ngo1k59@hcmut.edu.vn>
2023-05-03 10:37:11 +0700

    change nav bar for log in, log out

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,4 +1,5 @@
 <?php 
+use \app\core\Application;
 
 ?>
 
@@ -10,7 +11,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>HomepageLogin</title>
+    <title>Homepage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   </head>
   <body>
@@ -29,21 +30,27 @@
             <a class="nav-link" href="/contact">Contact</a>
           </li>
         </ul>
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/login">Login</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/register">Register</a>
-          </li>
-        </ul>
+        <?php if (Application::$app->isGuest()):  ?>
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/login">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/register">Register</a>
+            </li>
+          </ul>
+        <?php else:  ?>
+            <a class="nav-link active" aria-current="page" href="/logout">Welcome to <?php echo Application::$app->user->getDisplayName();?>
+              {Logout}
+            </a>
+        <?php endif; ?>
       </div>
     </div>
   </nav>
   <div class="container">
-    <?php if(\app\core\Application::$app->session->getFlash('success')): ?>
+    <?php if(Application::$app->session->getFlash('success')): ?>
     <div class="alert alert-success">
-      <?php echo \app\core\Application::$app->session->getFlash('success'); ?>
+      <?php echo Application::$app->session->getFlash('success'); ?>
     </div>
     <?php endif;?>
     {{content}}
