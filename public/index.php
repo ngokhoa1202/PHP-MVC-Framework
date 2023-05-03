5066eb87cf6863b4c89ed4984b29a09bf8ab4c00
Khoa Ngo <khoa.ngo1k59@hcmut.edu.vn>
2023-05-03 10:36:19 +0700

    change get logout

--- public/index.php
+++ public/index.php
@@ -1,18 +1,25 @@
 <?php 
 
 use app\core\Application;
+use app\models\User;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
 $config = [
+  "userClass" => User::class,
   "db" => [
     "dsn" => $_ENV["DB_DSN"],
     "username" => $_ENV["DB_USER"],
     "password" => $_ENV["DB_PASSWORD"],
   ]
   ]; // .env file
+
+error_reporting(E_ALL & ~E_DEPRECATED);
+//                      ^ this on
+// trigger_error('Deprecated', E_USER_DEPRECATED);
 
 $app = new Application(dirname(__DIR__), $config);
 
@@ -26,7 +33,7 @@ $app->router->post("/contact", [\app\controllers\SiteController::class, "handleC
 $app->router->get("/login", [\app\controllers\AuthController::class, "login"]);
 $app->router->post("/login", [\app\controllers\AuthController::class, "login"]);
 $app->router->get("/register", [\app\controllers\AuthController::class, "register"]);
-$app->router->post("/register", [\app\controllers\AuthController::class, "register"]);
+$app->router->get("/logout", [\app\controllers\AuthController::class, "logout"]);
 
 $app->run();
 ?>
