e10608ca19f6f3f9aa2e38bd4ceb042ce6cfdc13
Khoa Ngo <khoa.ngo1k59@hcmut.edu.vn>
2023-05-03 10:31:27 +0700

    Implement set, get and remove for a session when logging in

--- core/Session.php
+++ core/Session.php
@@ -36,5 +36,17 @@ class Session {
 
     $_SESSION[self::FLASH_KEY] = $flashMessages;
   }
+
+  public function set($key, $value) {
+    $_SESSION[$key] = $value;
+  }
+
+  public function get($key) {
+    return $_SESSION[$key] ?? false;
+  }
+
+  public function remove($key) : void {
+    unset($_SESSION[$key]);
+  }
 }
 ?>
