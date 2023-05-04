7e1d7bac9093598ce5bd7b80d41883b328f2d0e6
Khoa Ngo <khoa.ngo1k59@hcmut.edu.vn>
2023-05-04 10:34:23 +0700

    Set default attribute layout, array of BaseMiddleware. RegisterMiddleware method for push middleware

--- core/Controller.php
+++ core/Controller.php
@@ -1,9 +1,17 @@
 <?php 
 namespace app\core;
 use app\core\Application;
+use app\core\middlewares\BaseMiddleware;
 
 class Controller {
   protected string $layout = 'main';
+  public string $action = '';
+
+  /**
+   * @var \app\core\middlewares\BaseMiddleware[]
+   */
+  protected array $middlewares = []; 
+
   public function render($view, $params = []) {
     return Application::$app->router->renderView($view, $params);
   }
@@ -16,5 +24,16 @@ class Controller {
     return $this->layout;
   }
 
+  /**
+   * add middleware to middlewares array
+   */
+
+  public function registerMiddleware(BaseMiddleware $middleware) {
+    $this->middlewares[] = $middleware;
+  }
+
+  public function getMiddlewares() : array {
+    return $this->middlewares;
+  }
 };
 ?>
