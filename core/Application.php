2f35f579fb7944603e0ba804bf299dc13e9a5cd3
Khoa Ngo <khoa.ngo1k59@hcmut.edu.vn>
2023-05-04 10:33:02 +0700

    Modify controller may be nullable. Try, catch in run method.

--- core/Application.php
+++ core/Application.php
@@ -1,17 +1,24 @@
 <?php 
 namespace app\core;
+
+use \app\core\models\User;
+use Exception;
+
 /**
  * @package: app\core
  */
 class Application {
   public static string $ROOT_DIR;
+  public string $userClass;
   public Router $router;
   public Request $request;
   public Response $response;
   public Session $session;
   public static Application $app;
   public Database $database;
-  public Controller $controller;
+  public ?DBModel $user; // maybe a guest => null
+  public ?Controller $controller = null;
+  public string $layout = 'main';
   
   public function __construct($rootPath, array $config) {
     self::$ROOT_DIR = $rootPath;
@@ -20,7 +27,23 @@ class Application {
     $this->router = new Router($this->request, $this->response);
     $this->session = new Session();
     $this->database = new Database($config["db"]);
+    $this->userClass = $config['userClass'];
     self::$app = $this;
+
+    // reload page => keep record of one user 
+    $primaryValue = $this->session->get('user');
+
+    if ($primaryValue) {
+      $primaryKey = (new $this->userClass())->primaryKey();
+    
+      $this->user = (new $this->userClass())->findOne([
+        $primaryKey => $primaryValue
+      ]); 
+    }
+    else {
+      $this->user = null;
+    }
+    
   }
 
   public function get() {
@@ -28,7 +51,15 @@ class Application {
   }
 
   public function run() {
-    $this->router->resolve();
+    try {
+      $this->router->resolve();
+    }
+    catch (Exception $exception) {
+      $this->router->renderView('_error', [
+        'exception' => $exception
+      ]);
+    }
+    
   }
 
   public function getController() : Controller {
@@ -37,6 +68,27 @@ class Application {
 
   public function setController($controller) : void {
     $this->controller = $controller;
+  }
+
+  /**
+   * @param $user: actually a child class of DBModel abstract class
+   * @return: set session[key] = value of logined user.
+   */
+  public function login(DBModel $user) {
+    $this->user = $user;
+    $primaryKey = $user->primaryKey(); // 'id'
+
+    $primaryValue = $user->{$primaryKey}; // user.id = 1
+    $this->session->set('user', $primaryValue);
+  }
+
+  public function logout() {
+    $this->user = null;
+    $this->session->remove('user');
+  }
+
+  public static function isGuest() {
+    return !Application::$app->user;
   }
 };
 ?>
